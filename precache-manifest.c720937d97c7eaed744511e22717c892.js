5621d8993c44a7c0bacc07a5e3221fbdb1a979ba
Jemal Ahmedov <jahmedov@Jemals-MBP.lan>
2019-02-18 22:10:41 +0000

    Updates

---
+++ precache-manifest.c720937d97c7eaed744511e22717c892.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rss-dashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "ec43affe55125ca436f2",
+    "url": "/rss-dashboard/static/css/main.bdfdf52f.chunk.css"
+  },
+  {
+    "revision": "77364dcb0f2ab155f051",
+    "url": "/rss-dashboard/static/js/runtime~main.77364dcb.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rss-dashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7d873e0b7245389d1e1a",
+    "url": "/rss-dashboard/static/js/2.7d873e0b.chunk.js"
+  },
+  {
+    "revision": "ec43affe55125ca436f2",
+    "url": "/rss-dashboard/static/js/main.ec43affe.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rss-dashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rss-dashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rss-dashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7d873e0b7245389d1e1a",
+    "url": "/rss-dashboard/static/css/2.7752cf7f.chunk.css"
+  },
+  {
+    "revision": "50273b36f7bbcbccd62c76d0155463af",
+    "url": "/rss-dashboard/index.html"
+  }
+];
